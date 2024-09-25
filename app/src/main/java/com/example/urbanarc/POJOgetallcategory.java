2bce5dab6d7751970aa8432969ffd81cfa0e2663
Parth200611 <parthdahapute7@gmail.com>
2024-09-25 16:05:31 +0530

    usercategoryFragment  done

---
+++ app/src/main/java/com/example/urbanarc/POJOgetallcategory.java
@@ -0,0 +1,38 @@
+package com.example.urbanarc;
+
+public class POJOgetallcategory {
+    //pojo plane old java object
+    //reusablity
+    //get and set multiple data
+    String Id,CategoryImage,CategoryName;
+
+    public POJOgetallcategory(String id, String categoryImage, String categoryName) {
+        Id = id;
+        CategoryImage = categoryImage;
+        CategoryName = categoryName;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getCategoryImage() {
+        return CategoryImage;
+    }
+
+    public void setCategoryImage(String categoryImage) {
+        CategoryImage = categoryImage;
+    }
+
+    public String getCategoryName() {
+        return CategoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        CategoryName = categoryName;
+    }
+}
